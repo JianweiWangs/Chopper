2a5a01a7bd0399ecad693072f3d75c1b3a56caa2
wangfei <wangfei@zhihu.com>
2018-11-28 15:06:10 +0800

    update example

--- Example/Chopper/ViewController.swift
+++ Example/Chopper/ViewController.swift
@@ -13,19 +13,58 @@ import Chopper
 extension ViewController: JavaScriptModuleInterface {
     var moduleMapping: [String : Dispatch] {
         return [
-            "changeColor" : { message, callback in
+            "showAlert" : { message, callback in
                 print(message)
                 let frame = message.context.frameViewController
-                frame.view.backgroundColor = .gray
-                callback()
+                let title = message.params?["title"]
+                let message = message.params?["message"]
+                let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                let alertAction = UIAlertAction(title: "确定", style: .cancel, handler: nil)
+                alert.addAction(alertAction)
+                frame.present(alert, animated: true, completion: nil)
             }
         ]
     }
-
     var module: String {
         return "test"
     }
+}
 
+class ViewController: UIViewController  {
+
+    @IBOutlet weak var webview: WKWebView!
+
+    var jsbridge: JavaScriptBridge!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        webview.load(URLRequest(url: URL(string: "https://www.baidu.com")!))
+        webView.uiDelegate = self
+        jsbridge = JavaScriptBridge(dataSource: self)
+        DispatchQueue.main.asyncAfter(deadline: .now() + 5) {
+            self.webView.evaluateJavaScript("""
+window.webkit.messageHandlers.chopperNative.postMessage({
+    'module':'test',
+    'action':'showAlert',
+    'callbackID': 'ghuhygvbu87ygbuij8uhy',
+    'params': {
+        'title': 'Chopper',
+        'message': '这是一次 js call native 的测试'
+    }
+})
+
+""", completionHandler: nil)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    deinit {
+        print("释放了")
+    }
 }
 
 
@@ -43,31 +82,6 @@ extension ViewController : JavaScriptBridgeDataSource {
         return [self]
     }
 
-}
-
-class ViewController: UIViewController  {
-
-    @IBOutlet weak var webview: WKWebView!
-
-    var jsbridge: JavaScriptBridge!
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        webview.load(URLRequest(url: URL(string: "https://www.baidu.com")!))
-        webView.uiDelegate = self
-        jsbridge = JavaScriptBridge(dataSource: self)
-
-
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    deinit {
-        print("释放了")
-    }
 }
 
 extension ViewController: WKUIDelegate {
